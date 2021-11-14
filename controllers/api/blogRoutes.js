8bee6c03516dfc79ca062feef3cb43dd6b483275
MIRANDA <miranda.zermeno4@gmail.com>
2021-11-14 17:44:19 -0600

    added starter code to blogRoutes.js

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,40 @@
+// Pulled starter code from mini project
+
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', async (req, res) => {
+    try {
+      const newProject = await Blog.create({
+        ..req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newProject);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogData = await Blog.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogData) {
+      res.status(404).json({ message: 'Post cannot be found!' });
+      return;
+    }
+
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
